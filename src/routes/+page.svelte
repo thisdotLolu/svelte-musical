ea61abac8d3f30da8843b259fe8525f774fc6756
thisdotlolu <davidebe63@gmail.com>
2024-11-04 13:18:03 +0100

    feat:spotify login

---
+++ src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<input/>
+<select name="" id="">
+    <option value=""></option>
+</select>
+<button>Button</button>
+
+
+
+<style lang="scss">
+    @use '@unsass/breakpoint';
+    @use '../styles/_function.scss';
+    h1{
+        color:red;
+        @include breakpoint.up('md'){
+            color: green;
+        }
+    }
+</style>
---
+++ src/routes/api/auth/callback/+server.ts
@@ -0,0 +1,44 @@
+import { APP_URL, SPOTIFY_APP_CLIENT_ID, SPOTIFY_APP_CLIENT_SECRET } from "$env/static/private";
+import { error, redirect, type RequestHandler } from "@sveltejs/kit";
+
+export const GET:RequestHandler=async({url,cookies,fetch})=>{
+    const code = url.searchParams.get('code') || null;
+    const state = url.searchParams.get('state') || null;
+    
+    const storedState = cookies.get('spotify_auth_state') || null;
+    const storedChallengeVerifier = cookies.get('spotify_auth_challenge_verifier') || null;
+
+    console.log("stored:",storedState)
+    console.log("state:",state)
+
+    if(state === null || state !== storedState){
+        throw error(400, "State Mismatch!");
+    }
+
+    const response = await fetch('https://accounts.spotify.com/api/tokens',{
+        method:'POST',
+        headers:{
+            'Content-Type':'application/x-www-form-urlencoded',
+             Authorization: `Basic ${Buffer.from(`${SPOTIFY_APP_CLIENT_ID}:${SPOTIFY_APP_CLIENT_SECRET}`).toString('base64')}`
+        },
+        body: new URLSearchParams({
+            code:code || '',
+            redirect_uri : `${APP_URL}/api/auth/callback`,
+            grant_type:'authoraization_code',
+            code_cverifier:storedChallengeVerifier || '',
+            client_id: SPOTIFY_APP_CLIENT_ID
+        })
+    });
+    const responseJSON = await response.json();
+    if(responseJSON.error){
+        throw error(400, responseJSON.error_description);
+    }
+    
+
+    cookies.delete('spotify_auth_state',{path:'/'});
+    cookies.delete('spotify_auth_challenge_verifier',{path:'/'});
+    cookies.set('refresh_token', responseJSON.refresh_token, {path:'/'});
+    cookies.set('access_token', responseJSON.access_token, {path:'/'});
+
+    throw redirect(303, '/');
+}
