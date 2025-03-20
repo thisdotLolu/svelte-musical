a6c0141c795499788ea0ccb9c29c1390ae8a5ea6
thisdotlolu <davidebe63@gmail.com>
2025-03-20 12:43:30 +0100

    fix:page issue

--- src/routes/playlist/[id]/+page.svelte
+++ src/routes/playlist/[id]/+page.svelte
@@ -1,8 +1,29 @@
 <script lang='ts'>
+  import ItemPage from "$assets/ItemPage.svelte";
   import type { PageData } from "./$types";
 
     export let data:PageData;
 
-    $:console.log(data)
+    $:console.log("dat",data)
+
+    $:playlist = data.playlist;
+    $: tracks = data.playlist.tracks;
+
+    console.log("pl",playlist)
+
+    let filteredTracks: SpotifyApi.TrackObjectFull[];
+
+    $:{
+        filteredTracks = []
+        tracks.items.forEach(item=>{
+            if(item.track) filteredTracks = [...filteredTracks, item.track]
+        })
+    }
 
 </script>
+
+<ItemPage title={playlist.name}
+image={playlist.images.length > 0 ? playlist.images[0].url : undefined }
+>
+    
+</ItemPage>
---
+++ src/routes/playlist/[id]/+page.ts
@@ -0,0 +1,37 @@
+// import { fetchRefresh } from '$helpers';
+import { fetchRefresh } from '$lib/helpers';
+import { error } from '@sveltejs/kit';
+import type { PageLoad } from '../../$types';
+// import type { PageLoad } from './$types';
+
+export const load: PageLoad = async ({ fetch: _fetch, params, depends, route }) => {
+	depends(`app:${route.id}`);
+	const fetch = (path: string) => fetchRefresh(_fetch, path);
+
+	const playlistRes = await fetch(`/api/spotify/playlists/${params.id}`);
+
+	if (!playlistRes.ok) {
+		throw error(playlistRes.status, 'Failed to load playlist!');
+	}
+
+	const playlistResJSON: SpotifyApi.SinglePlaylistResponse = await playlistRes.json();
+
+	let color = null;
+	if (playlistResJSON.images.length > 0) {
+		const colorRes = await fetch(
+			`/api/average-color?${new URLSearchParams({
+				image: playlistResJSON.images[0].url
+			}).toString()}`
+		);
+
+		if (colorRes.ok) {
+			color = (await colorRes.json()).color;
+		}
+	}
+
+	return {
+		playlist: playlistResJSON,
+		color,
+		title: playlistResJSON.name
+	};
+};
